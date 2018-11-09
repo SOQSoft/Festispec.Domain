a514297bf64f5d3c7bb7dbd8d0bc5d52a33b5098
Guus van der Meer <guus.meer@outlook.com>
2018-11-09 12:39:35 +0100

    Annotationsfix

--- src/Form.cs
+++ src/Form.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Festispec.Database.Models
 {
     public partial class Form
-    {       
+    {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsTemplate { get; set; }
--- src/Question.cs
+++ src/Question.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Festispec.Domain.src;
 
 namespace Festispec.Database.Models
 {
-    public partial class Question
-	{
-		public int Id { get; set; }
+    public class Question
+    {
+        [Key]
+        public int Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
         public QuestionType QuestionType { get; set; }
         public int? FormId { get; set; }
         public Form Form { get; set; }
         public ICollection<QuestionItem> QuestionItem { get; set; } = new List<QuestionItem>();
-	}
+    }
 }
