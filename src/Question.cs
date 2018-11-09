db60d2b36f3c85dfeece00bc98da0b6573079247
Guus van der Meer <guus.meer@outlook.com>
2018-11-09 12:28:38 +0100

    QuestionType update

--- src/Question.cs
+++ src/Question.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using Festispec.Domain.src;
 
 namespace Festispec.Database.Models
 {
@@ -8,9 +9,8 @@ namespace Festispec.Database.Models
 		public int Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
-        public int QuestionType { get; set; }
+        public QuestionType QuestionType { get; set; }
         public int? FormId { get; set; }
-
         public Form Form { get; set; }
         public ICollection<QuestionItem> QuestionItem { get; set; } = new List<QuestionItem>();
 	}
---
+++ src/QuestionType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Festispec.Domain.src
+{
+    public enum QuestionType
+    {
+        Numerical, 
+        Multiplechoice, 
+        Options, 
+        Map,
+        Picture,
+        Table, 
+        Open,
+        Draw,
+        Instructions, 
+
+
+    }
+}
